6f7cbe889c9f0f0093ee08b7e0efa6c74b341cbc
junior-delatorre <jrdelatorre7890@gmail.com>
2019-02-04 08:55:34 -0800

    aa

---
+++ gitpush.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+git add -A;
+git commit -m "aa";
+git push;
+junior-delatorre;
+jrdelatorre1@;
---
+++ junior-delatorre.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  junior-delatorre;
+git config --global user.name  junior;
+git config --global user.user junior-delatorre;
+git config --global user.email jrdelatorre7890@gmail.com;
+git config --list;
